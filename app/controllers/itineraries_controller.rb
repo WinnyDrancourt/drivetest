6ac6f088905a468394caa341e7beeeec8a3e0319
WinnyDrancourt <winny.morel@orange.fr>
2024-03-21 11:53:36 +0100

    controller
    Itineraries
    Destinations

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -4,6 +4,14 @@ class ItinerariesController < ApplicationController
   def show; end
 
   def new
-    @user
+    @itinerary = Itinerary.new
+    @itinerary.destinations.build
+  end
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:title, :start_date, :end_date,
+                                      destination_attributes: %i[city notes staying_time])
   end
 end
