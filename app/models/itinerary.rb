bde3570a1fb3efa1f1fe1ae086f531f288a43d11
WinnyDrancourt <winny.morel@orange.fr>
2024-03-22 14:40:28 +0100

    Change Map
    search box
    add marker

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,7 +1,7 @@
 class Itinerary < ApplicationRecord
   belongs_to :user
   has_many :destinations, dependent: :destroy
-  accepts_nested_attributes_for :destinations
+  accepts_nested_attributes_for :destinations, allow_destroy: true, reject_if: :all_blank
   has_many :likes
 
   after_initialize :build_default_destination
