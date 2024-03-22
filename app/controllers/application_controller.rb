51ea63e5768a7052cbc3a170f7c5ed3fa0ea5672
Shaun Chowthee <sn.chowthee@gmail.com>
2024-03-22 11:14:19 +0100

    called iti_count method in footer via appcontroller, created def show in users controllers, added a link to itinerary in users/show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_user
+  before_action :itineraries_count
+
   def set_user
     @user = current_user
+  end
+
+  def itineraries_count
+    @iti_count = Itinerary.all.size
   end
 end
