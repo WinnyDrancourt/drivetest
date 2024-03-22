018593ebb338f4c95cf906155510706edf4b398f
Shaun Chowthee <sn.chowthee@gmail.com>
2024-03-22 12:53:36 +0100

    fixing application controller and move method to users controllers, formatting dates on the users view and added links to users cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :itineraries_count
 
   def set_user
-    if params[:id].present?
-      begin
-        @user = User.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-        flash[:alert] = "User doesn't exist... yet!"
-        redirect_to root_path
-      end
-    else
       @user = current_user
-    end
-  end
-
-  def itineraries_count
-    @iti_count = Itinerary.all.size
   end
 
   def itineraries_count
